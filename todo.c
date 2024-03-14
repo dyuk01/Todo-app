a39bcd93298074ddb96d2e2ddb33654fabf7db8b
Peter Yuk <peteryuk91@gmail.com>
2024-03-14 17:38:18 -0400

    Update todo.c

--- todo.c
+++ todo.c
@@ -23,5 +23,33 @@ void init(struct List *list) {
 }
 
 void addTask(struct List *list, const char *task) {
+    if (list->count < MAX_TASK) {
+        strcpy(list->tasks[list->count].desc, task);
+        list->count++;
+    }
+    else {
+        printf("List is full!\n");
+    }
+}
+
+void removeTask(struct List *list, const char *task) {
+    int flag = 0;
+    for (int i = 0; i < list->count; i++) {
+        if (strcmp(list->tasks[i].desc, task) == 0) {
+            strcpy(list->tasks[i].desc, "");
+            list->tasks[i].isDone = 0;
+            flag++;
+        }
+    }
+    if (flag == 0) {
+        printf("Task cannot be found!\n");
+    }    
+}
+
+void markDone(struct List *list, int index) {
+    
+}
+
+void markUnDone(struct List *list, int index) {
 
 }
